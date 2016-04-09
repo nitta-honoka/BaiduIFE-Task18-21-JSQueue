84dc1f5dfbca45cd1390d5185257590b45653367
honoka <xal821792703@163.com>
2016-04-09 23:45:13 +0800

    feat(browser): complete task18

--- task18/js/main.js
+++ task18/js/main.js
@@ -21,26 +21,52 @@
             return event.target || event.srcElement;
         }
     };
+    //DOM操作辅助对象
+    var DomUtil = {
+      create: function (num, className) {
+          var divElement = document.createElement('div');
+          divElement.innerText = num.toString();
+          divElement.className = className;
+          return divElement;
+      },
+        insertEle: function (containId, element, type) {
+            var containEle = document.getElementById(containId);
+            var childList = containEle.childNodes;
+            if (type === 'left') {
+                containEle.insertBefore(element, childList[0]);
+            } else {
+                containEle.appendChild(element);
+            }
+        },
+        removeEle: function (containId, type) {
+            var containEle = document.getElementById(containId);
+            if (type === 'left') {
+                containEle.removeChild(containEle.firstChild);
+            } else {
+                containEle.removeChild(containEle.lastChild);
+            }
+        }
+    };
     //将点击事件代理在输入区域父元素上
     var body = document.getElementById('input-area');
     var val = document.getElementById('num');
-    var num;
     EventUtil.addHandler(body, "click", function (event) {
-        num = val.value;
+        var num = val.value;
         event = EventUtil.getEvent(event);
         var target = EventUtil.getTarget(event);
+        var numEle = DomUtil.create(num, 'box');
         switch (target.id) {
             case "in-left":
-                console.log(num);
+                DomUtil.insertEle('container', numEle, 'left');
                 break;
             case "in-right":
-                console.log("in-right");
+                DomUtil.insertEle('container', numEle, 'right');
                 break;
             case "rm-left":
-                console.log("rm-left");
+                DomUtil.removeEle('container', 'left');
                 break;
             case "rm-right":
-                console.log("rm-right");
+                DomUtil.removeEle('container', 'right');
                 break;
         }
     });
